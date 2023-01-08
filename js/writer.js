1038ad1f621e4ab209ccefb8f47eac653698d35d
Nikhil Gupta <gnikhil@email.com>
2023-01-08 11:43:54 +0530

    Writer problems

--- js/writer.js
+++ js/writer.js
@@ -3,12 +3,12 @@ const waitTime = 1000;
 let l_text, ci=0, stopped=false;
 function writeString(e, str, i) {
     e.innerHTML = e.innerHTML + str[i];
-    if(e.innerHTML == str)
+
+    if(e.innerText == str)
     {   console.log("here");
         if(ci++==l_text.length)ci=0;
         setTimeout(slowlyDelete, waitTime, e);
     }
-        
 }
   
 function deleteString(e) {
@@ -19,15 +19,17 @@ if (e.innerHTML.length == 0)
 }
 
 function slowlyDelete(e) {
-    if(stopped)return;
-    stopped=true;
-for (var i = 0; i < e.innerHTML.length; i++) {
+    // if(stopped)return;
+    // stopped=true;
+for (var i = 0; i < e.innerText.length; i++) {
     setTimeout(deleteString, typSpd / 2 * i, e);
 }
+
 }
 
 function slowlyWrite(e, str) {
     // if(str != undefined)
+console.log("Called Slowly write")
 for (var i = 0; i < str.length; i++) {
     setTimeout(writeString, typSpd * i, e, str, i);
 }
